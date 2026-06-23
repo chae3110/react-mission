89882e22422f29b4276a26e2cd5cf24a7a529449
chae <dldmd33333@gmail.com>
2026-06-23 16:33:47 +0900

    과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import Card from "./components/Card";
+import ProductCard from "./components/Card";
 
 function App() {
-  const product = {
-    name: "무선 키보드",
-    price: 39000,
-    brand: "EZTECH",
-  };
+  const products = [
+    { id: 1, name: "무선 키보드", price: 39000, brand: "EZTECH" },
+    { id: 2, name: "무선 마우스", price: 29000, brand: "EZTECH" },
+    { id: 3, name: "모니터", price: 199000, brand: "EZTECH" },
+  ];
 
-  return <Card product={product} />;
+  const list = products.map((p) => (
+    <ProductCard key={p.id} name={p.name} price={p.price} brand={p.brand} />
+  ));
+
+  return <>{list}</>;
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,11 @@
-export default function Card({ product }) {
+import styles from "./ProductCard.module.css";
+
+export default function ProductCard({ name, price, brand }) {
   return (
-    <div>
-      <h2>{product.name}</h2>
-      <p>{product.price}</p>
-      <p>{product.brand}</p>
-    </div>
+    <article className={styles.cardItem}>
+      <h3>{name}</h3>
+      <p>{price}</p>
+      <p>{brand}</p>
+    </article>
   );
 }
